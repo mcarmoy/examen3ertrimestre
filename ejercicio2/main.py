99f553ea84cda0149c2ea88ee93785e4be539f7b
Miguel Ángel Carrasco Moya <mcarmoy2401@g.educaand.es>
2026-04-28 11:31:00 +0200

    Ejercicio 2 realizado de mi manera para seguir

---
+++ ejercicio2/main.py
@@ -0,0 +1,13 @@
+"""
+Programa que mediante una función debería entrar en el archivo datos.txt, coger su conteneido.
+Después mostrarlo por pantalla del color que le indiques en el print.
+Extra: Para continuar y que me puntue el ejercicio he hehco el programa sin entrar en el archivo para no dejarlo a medias y seguir.
+Al no tener función tampoco puedo documentarla pero sería este formato.
+"""
+from colorama import Fore
+
+texto = "Hola desde un archivo"
+
+print(Fore.GREEN + texto)
+
+    
